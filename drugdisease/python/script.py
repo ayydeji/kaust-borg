b0a6577c873e30c644b5df5a634c4a6cffc42e58
Abdul Kamal <abdulmkamal@hotmail.com>
2020-03-06 14:20:49 +0300

    Update script.py

--- drugdisease/python/script.py
+++ drugdisease/python/script.py
@@ -7,10 +7,12 @@ import sys
 import math
 
 def edit_fatsa():
+    # edit file path here to relevent drug-target tsv you want to generate.
     with open('/home/muhammaa/ilp/original-data/targets.csv', 'w', newline='') as targets:
         fieldnames = ['drug', 'target']
         writer = csv.DictWriter(targets, fieldnames=fieldnames)
         writer.writeheader()
+        # edit file path here to the drugbank target file.
         for record in SeqIO.parse('/home/muhammaa/ilp/original-data/protein.fasta', 'fasta'):
             prot = record.id.split('|')[1]
             drugs = record .description
@@ -21,6 +23,7 @@ def edit_fatsa():
 
 def edit_ppi():
     with open('/home/muhammaa/ilp/original-data/ppi.csv', 'r') as ppi:
+        # edit here to file path of the STRING ppi file to edit.
         with open('/home/muhammaa/ilp/original-data/protein_protein.csv', 'w', newline='') as new_file:
             reader = csv.DictReader(ppi, delimiter=',')
             fieldnames=['prot_a', 'mode', 'prot_b']
@@ -36,12 +39,14 @@ def edit_ppi():
                         if val_a == 'NA' or val_b == 'NA':
                             continue
                         writer.writerow({'prot_a':val_a,'mode':row['mode'],'prot_b':val_b})
-
+# method for replaceing the ids used in the disgenet data with uniprot.
 def edit_gene_disease():
     vocab = pd.read_csv('/home/muhammaa/ilp/original-data/gene_mapping.tsv', encoding='utf-8', delimiter='\t')
     vocab_dict = dict(zip(vocab['GENEID'], vocab['UniProtKB']))
-
+    
+    
     with open('/home/muhammaa/ilp/original-data/curated_gene_disease_associations.tsv', 'r') as ppi:
+        # path to file with the converted genes.
         with open('/home/muhammaa/ilp/original-data/gene_disease.csv', 'w', newline='') as new_file:
             reader = csv.DictReader(ppi, delimiter='\t')
             fieldnames=['geneId', 'diseaseId']
@@ -56,6 +61,7 @@ def edit_gene_disease():
                         writer.writerow({'geneId':vocab_dict[row['geneId']],'diseaseId':row['diseaseId']})
 
 def pykeen_csv():
+    # path to where you want the final file to be sent to.
     with open('/home/muhammaa/ilp/graph/connected/final.csv', 'w', newline='') as final:
         writer = csv.writer(final, delimiter=',')
 
@@ -83,7 +89,9 @@ def pykeen_csv():
         for row in gg_reader:
             writer.writerow([row[0], 'interacts_with', row[1]])
 
-
+# when you have the pykeen finla file with all relations you can run this function to convert it to .kb.
+# outputfile= output dir to where you want the kb to be saved.
+# inputfile= input file of the tsv holding all your relationships.
 def make_kb(outputfile, inputfile, diseaseset=None, drugset=None):
     if drugset == None:
         drugset = set()
@@ -95,10 +103,12 @@ def make_kb(outputfile, inputfile, diseaseset=None, drugset=None):
         # final = open(inputfile, 'r')
         final_reader = pd.read_csv(inputfile, header=None, sep='\t')
         final_reader = final_reader.sort_values(by=[1], ascending=False)
+        # edit here to where your negatives file is.
         negatives = open('/home/muhammaa/ilp/original-data/new/negatives.csv', 'r')
         neg_reader = csv.reader(negatives)
 
         for index, row in final_reader.iterrows():
+            # change the target to the relation you are trying to predict.
             if str(row[1]).find('has_disease') != -1:
                 kb.write("{}('{}', '{}', pos).\n".format(row[1], row[0], row[2]))
                 if row[0] not in drugset:
@@ -111,7 +121,7 @@ def make_kb(outputfile, inputfile, diseaseset=None, drugset=None):
             if row[0] in drugset and row[2] in diseaseset:
                 kb.write("has_disease('{}', '{}', neg).\n".format(row[0], row[2]))
 
-
+#run this to get a subset of your dataset. k is the number of drugs to take.
 def subset(k=None, drugset=None, create_kb=True, split=False):
     if k is None and drugset is None:
         print("either enter a number or enter a drugset")
@@ -119,6 +129,7 @@ def subset(k=None, drugset=None, create_kb=True, split=False):
 
     if k is None:
         k=len(drugset)
+    # path to drug_gene dataset.
     drug_gene = open('/home/muhammaa/ilp/indevidual-graphs/drug_gene.csv', 'r')
     df = pd.read_csv(drug_gene, header=None)
     dg_dict = {}
@@ -160,13 +171,13 @@ def subset(k=None, drugset=None, create_kb=True, split=False):
                 else:
                     writer.writerow([drug, 'has_target', target])
                     gene_list.add(target)
-
+        # path to gene_gene set.
         gene_gene = open('/home/muhammaa/ilp/indevidual-graphs/gene_gene.csv', 'r')
         gg_reader = csv.reader(gene_gene)
-
+        # path to gene_disease dataset.
         gene_disease = open('/home/muhammaa/ilp/indevidual-graphs/gene_disease.csv', 'r')
         gd_reader = csv.reader(gene_disease)
-        
+        # path to gene_disease dataset.
         drug_disease = open('/home/muhammaa/ilp/indevidual-graphs/drug_disease.csv', 'r')
         dd_reader = csv.reader(drug_disease)
 
