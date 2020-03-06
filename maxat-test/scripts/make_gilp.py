6ef4ff01ecdcaa1cc49fec22a11a347a5cf895b2
Abdul Kamal <abdulmkamal@hotmail.com>
2020-03-06 15:17:04 +0300

    Update make_gilp.py

--- maxat-test/scripts/make_gilp.py
+++ maxat-test/scripts/make_gilp.py
@@ -1,13 +1,17 @@
+# gilps file that can be used with any tilde file to convert to gilps format.
 import csv
 import sys
+# start program with the path to where you want the file to be saved as the first argument, the .kb file as the second argument.
 def main():
     with open(sys.argv[1], 'w') as prolog:
         origin = open(sys.argv[2], 'r')
         lines = origin.readlines()
+        # swap with the relevent relations from your dataset. modeh point to the target and modeb to the background information.
         prolog.write("\n"
                      ":- modeh(1, links_with(+protein, -protein)).\n"
                      ":- modeb(*, has_annotation(+protein, -annotation)).\n")
         for line in lines:
+            # update with correct prediction target.
             if line.find('links_with') != -1:
                 if line.find('pos') != -1:
                     prolog.write('example({}), 1).\n'.format(line[:-8].strip()))
