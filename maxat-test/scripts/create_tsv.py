5132d37e66761a5e630469863ddf7bf4517809f3
Abdul Kamal <abdulmkamal@hotmail.com>
2020-03-06 15:13:32 +0300

    Update create_tsv.py

--- maxat-test/scripts/create_tsv.py
+++ maxat-test/scripts/create_tsv.py
@@ -1,9 +1,11 @@
 import csv
 import sys
 
+# in the command line specify the annotation file as the first argument and the link file as the second.
 assoc_file = sys.argv[1]
 link_file = sys.argv[2]
 
+# path to where you wnat to save the fie.
 with open('/home/muhammaa/ilp/maxat-test/pykeen/graph/all_train_human_go.tsv', 'w', newline='') as outfile:
     writer = csv.writer(outfile, delimiter='\t')
     
@@ -19,7 +21,7 @@ with open('/home/muhammaa/ilp/maxat-test/pykeen/graph/all_train_human_go.tsv', '
 
     for row in link_reader:
         writer.writerow([row[0], 'links_with', row[1]])
-
+    # path to go annotations.
     go_file = open('/home/muhammaa/ilp/maxat-test/data/all_goslim_yeast.tsv', 'r')
     go_reader = csv.reader(go_file, delimiter='\t')
 
